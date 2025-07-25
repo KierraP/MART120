ebd889333ec61056923ca0dc7c5067cbd959bd43
Kierra Plaggemeyer <k.idelle13@gmail.com>
2025-07-25 16:34:11 -0600

    Added files

---
+++ Homework 14 Final/Self-Portrait.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(150);
+  rect(250,100,50,80);
+  rect(200,100,50,80);
+  circle(250,100,100);
+  circle(235,95,5);
+  circle(265,95,5);
+  line(250,100,250,120);
+  point(250,130);
+  point(252,130);
+  point(248,130);
+  point(254,129);
+  point(246,129);
+  point(244,128);
+  point(256,128);
+  line(250,250,250,150);
+  line(250,170,170,250);
+  line(320,250,250,170);
+  triangle(170,250,120,270,190,290);
+  triangle(320,250,370,270,310,290);
+  line(250,250,340,450);
+  line(250,250,180,450);
+  rect(340,450,70,30);
+  rect(110,450,70,30);
+  text('Self-Portrait',10,30)
+  text('by Kierra',440,30)
+}
